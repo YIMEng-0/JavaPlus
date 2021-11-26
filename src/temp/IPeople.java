a44fc6aaa2bec270029f7f0406a91a815d754177
YIMEng-0 <1546946428@qq.com>
2021-11-26 10:54:24 +0800

    20211125

---
+++ src/temp/IPeople.java
@@ -0,0 +1,11 @@
+package temp;
+
+/**
+ * @author Doraemon
+ * @date 2021/11/26 10:29 上午
+ * @version 1.0
+ */
+public interface IPeople {
+    public static final double s = 123;
+
+}
---
+++ src/temp/TestAbstract.java
@@ -0,0 +1,10 @@
+package temp;
+
+/**
+ * @author Doraemon
+ * @date 2021/11/26 10:26 上午
+ * @version 1.0
+ */
+public abstract class TestAbstract {
+    public abstract void tetstst();
+}
